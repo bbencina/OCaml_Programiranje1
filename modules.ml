47e433ce97718d7443fbfc6d9b844ccbc4076065
bbencina <benjamin.bencina@gmail.com>
2018-07-03 21:18:42 +0200

    Finished the modules exercises in preparation of exam.

--- modules.ml
+++ modules.ml
@@ -71,7 +71,7 @@ module NAT_int : NAT = struct
   let zero = 0
   let one = 1
   let add a b = a + b
-  let sub a b = if a < b 
+  let sub a b = if a < b
 		then zero
 		else a - b
   let mul a b = a * b
@@ -101,11 +101,11 @@ module NAT_P : NAT = struct
 	| (Zero, Succesor _) -> false
 	| (Succesor _, Zero) -> false
 	| (Succesor x, Succesor y) -> eq x y
-	
+
   let zero = Zero
-  
+
   let one = Succesor Zero
-  
+
   let rec add a b =
 	match a, b with
 	| (x, Zero) -> x
@@ -117,11 +117,11 @@ module NAT_P : NAT = struct
 	| (x, Zero) -> x
 	| (Succesor x, Succesor y) -> sub x y
 
-  let rec mul a b = 
+  let rec mul a b =
 	match a, b with
 	| (_, Zero) | (Zero, _) -> Zero
 	| (x, Succesor y) -> add x (mul x y)
-	
+
   let int_to_nat a = if a < 0
 	then Zero
 	else
@@ -130,7 +130,7 @@ module NAT_P : NAT = struct
 		| 0 -> acc
 		| x -> inting (x-1) (Succesor acc)
 	in inting a Zero
-	
+
   let nat_to_int a =
 	let rec natting nat acc =
 		match nat with
@@ -146,12 +146,21 @@ end
    We will need a carrier type, a test for equality, zero, one, i, negation and
    conjugation, addition, multiplication, division, and taking the inverse.
  *)
-(* module type COMPLEX = sig
+module type COMPLEX = sig
     type t
     val eq : t -> t -> bool
-    ...
+    val zero : t
+    val one : t
+    val i : t
+    val neg : t -> t
+    val con : t -> t
+    val add : t -> t -> t
+    (*val sub : t -> t -> t*)
+    val mul : t -> t -> t
+    val div : t -> t -> t
+    val inv : t -> t
   end
- *)
+
 
 (* Write an implementation of Professor Descartes's complex numbers. Reminder:
  this should be the cartesian representation (latin_of_french "Descartes" =
@@ -160,17 +169,31 @@ end
   Recommendation: implement a few basic parts of the module but leave division
   for later. It's relatively messy.
  *)
-(*
+
 module Cartesian : COMPLEX = struct
 
   type t = {re : float; im : float}
 
-  let eq x y = x.re = y.re && ...
+  let eq x y = x.re = y.re && x.im = y.im
 
-  ...
+  let zero = {re=0.0; im=0.0}
+  let one = {re=1.0; im=0.0}
+  let i = {re=0.0; im=1.0}
+
+  let neg x = {re = (-1.0) *. x.re; im = (-1.0) *. x.im}
+  let con x = {re =  x.re; im = (-1.0) *. x.im}
+
+  let add x y = {re = x.re +. y.re; im = x.im +. y.im}
+  (*let sub x y = {re = x.re -. y.re; im = x.im -. y.im}*)
+  let mul x y = {re = x.re *. y.re -. x.im *. y.im;
+                im = x.re *. y.im +. x.im *. y.re}
+  let div x y = {re = (x.re *. y.re +. x.im *. y.im) /. (y.re *. y.re +. y.im *. y.im);
+                im = (y.re *. x.im -. x.re *. y.im) /. (y.re *. y.re +. y.im *. y.im)}
+  let inv x = {re = x.re /. (x.re *. x.re +. x.im *.x.im);
+              im = x.im /. (x.re *. x.re +. x.im *.x.im)}
 
 end
- *)
+
 
 
 (* Now implement Professor Bessel's complex numbers. The carrier this time
@@ -181,18 +204,39 @@ end
    multiplication. Then the rest except for addition. So far, so pleasant.
    Finally implement addition. Now form an opinion on why nobody likes polar
    coordinates. *)
-(*
+
 module Polar : COMPLEX = struct
 
   type t = {magn : float; arg : float}
 
   let pi = 2. *. acos 0.
-  let rad deg = (deg /. 180.) *. pi
-  let deg rad = (rad /. pi) *. 180.
+  let rad_of_deg deg = (deg /. 180.) *. pi
+  let deg_of_rad rad = (rad /. pi) *. 180.
 
-  let eq x y =
+  let zero = {magn=0.0; arg=0.0}
+  let one = {magn=1.0; arg=0.0}
+  let i = {magn=1.0; arg = pi /. 2.0}
 
-  ...
+  let eq x y = x.magn = y.magn && x.arg = y.arg
+
+  let neg x = {magn = x.magn; arg = if x.arg >= pi
+                                    then x.arg -. pi
+                                    else x.arg +. pi}
+  let con x = {magn = x.magn; arg = 2.0 *. pi -. x.arg}
+
+  let add x y = {magn = sqrt (x.magn *. x.magn +. y.magn *. y.magn +. 2. *. x.magn *. y.magn *. cos (if x.arg -. y.arg < -1. *. pi then 2. *. pi +. x.arg -. y.arg else x.arg -. y.arg));
+                arg = atan ((x.magn *. sin x.arg +. y.magn *. sin y.arg) /. (x.magn *. cos x.arg +. y.magn *. cos y.arg))}
+  (*let sub x y = failwith "later" *)
+  let mul x y = {magn = x.magn *. y.magn;
+                arg = if x.arg +. y.arg >= 2.0 *. pi
+                      then x.arg +. y.arg -. 2.0 *. pi
+                      else x.arg +. y.arg}
+  let div x y = {magn = if y.magn <> 0.
+                        then x.magn /. y.magn
+                        else 0.0;
+                arg = if x.arg -. y.arg < 0.0
+                      then x.arg -. y.arg +. 2.0 *. pi
+                      else x.arg -. y.arg}
+  let inv x = {magn = 1.0 /. x.magn; arg = (-1.0) *. x.arg}
 
 end
- *)
